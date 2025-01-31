410c9b390891e30ed3e6d1d4b6d7342f639af953
Arnaud-bd <abaudrand@gaming.tech>
2025-01-31 18:31:30 +0100

    fin jour 1

--- src/Fracture/Components/Transform2D.cpp
+++ src/Fracture/Components/Transform2D.cpp
@@ -1,2 +1,8 @@
-
 #include "Transform2D.h"
+
+Transform2D::Transform2D(sf::Vector2f _position, float _scale, float _rotation)
+{
+	m_Position = _position;
+	m_Scale = _scale;
+	m_Rotation = _rotation;
+}
--- src/Fracture/GameComponents/Scene.cpp
+++ src/Fracture/GameComponents/Scene.cpp
@@ -1,24 +1,29 @@
 #include "Scene.hpp"
 #include "../Components/Behaviour.h"
+#include "../Components/Transform2D.h"
 Scene::Scene()
 {
 }
 
-uint64_t Scene::CreateEntity()
+Transform2D* Scene::CreateEntity(sf::Vector2f _position, float _scale, float _rotation)
 {
-	return 0;
+	Transform2D transform = {_position, _scale, _rotation};
+	
+	m_ComponentsList.push_back(&transform);
+
+	return &transform;
 }
 
 void Scene::Add(Component* _component)
 {
-	m_Components.push_back(_component);
+	m_ComponentsList.push_back(_component);
 }
 
 void Scene::Update()
 {
-	for (int i = 0; i < m_Components.size(); i++)
+	for (int i = 0; i < m_ComponentsList.size(); i++)
 	{
-		if (Behaviour* b = dynamic_cast<Behaviour*>(m_Components[i]))
+		if (Behaviour* b = dynamic_cast<Behaviour*>(m_ComponentsList[i]))
 			b->Update();
 	}
 }
