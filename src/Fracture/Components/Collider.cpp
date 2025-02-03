1325cd74fe55271ca74af3384aad076218ffd0dd
ThibautPat <tpatry@gaming.tech>
2025-02-03 13:39:06 +0100

    oui

--- src/Fracture/Components/Collider.cpp
+++ src/Fracture/Components/Collider.cpp
@@ -5,6 +5,38 @@ Collider::Collider()
 {
 }
 
+bool Collider::IsCollide(Collider _other)
+{
+    for (int i = 0; i < m_Hitboxs.size(); i++)
+    {
+        for (int j = 0; j < _other.m_Hitboxs.size(); j++)
+        {
+            sf::Vector2f distanceVector = _other.m_Hitboxs[j].getPosition() - m_Hitboxs[i].getPosition(); 
+
+            float distance = sqrt(pow(distanceVector.x, 2) + pow(distanceVector.y, 2));
+
+            if (distance <= m_Hitboxs[i].getRadius() + _other.m_Hitboxs[j].getRadius())
+                return true;
+        }
+    }
+    return false;
+}
+
+bool Collider::OnTriggerEnter(Collider _other)
+{
+    return false;
+}
+
+bool Collider::OnTriggerExit(Collider _other)
+{
+    return false;
+}
+
+bool Collider::OnTriggerStay(Collider _other)
+{
+    return false;
+}
+
 void Collider::Awake()
 {
 }
--- src/Fracture/Components/Collider.h
+++ src/Fracture/Components/Collider.h
@@ -5,12 +5,18 @@
 class Collider : public Component
 {
 	std::vector<sf::CircleShape> m_Hitboxs;
-	bool m_IsTrigger;
 
 public:
 	Collider();
+
+	bool IsCollide(Collider _other);
+
+	bool OnTriggerEnter(Collider _other);
+	bool OnTriggerExit(Collider _other);
+	bool OnTriggerStay(Collider _other);
 
 	void Awake() override;
 	void Start() override;
 };
 
+ 
